9a7b355b51a31067f614f0aec87679d7ad8245b5
tankhun <tankoon300950@gmail.com>
2023-06-06 13:48:30 +0700

    first app

---
+++ bin/dart_week4.dart
@@ -0,0 +1,11 @@
+
+void main() {
+  list<int> number = ['thailand','japan','korea'];
+  for (int i = 0; i <number.length; i++) {
+    print(number[i]);
+  }
+  // print(number[0])
+  // print(number[1])
+  // print(number[2])
+  // print(number[3])
+}
---
+++ bin/list.dart
@@ -0,0 +1,11 @@
+
+void main() {
+  List<String> vnl2023 = ["thailand","japan","korea"];
+
+  for (int index = 0; index < vnl2023.length; index++) {
+    print(vnl2023[index]);
+  }
+  // print(vnl2023[0])
+  // print(vnl2023[1])
+  // print(vnl2023[2])
+}
